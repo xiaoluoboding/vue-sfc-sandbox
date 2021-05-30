8b858b0a127d34e3a8932b6df5ad63a942d29093
xiaoluoboding <xiaoluoboding@gmail.com>
2021-05-30 12:02:48 +0800

    feat: change app dir

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,16 @@
+const path = require('path')
+const resolve = dir => path.join(__dirname, dir)
+
 module.exports = {
   publicPath: './',
+  pages: {
+    index: {
+      entry: resolve('app/main.ts')
+    }
+  },
   chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('app'))
     // load srcdoc.html only
     config.module
       .rule('raw')
